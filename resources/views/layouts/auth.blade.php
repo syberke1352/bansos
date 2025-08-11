6e3bf5769db6ec36fc15aac28eed8ef3168341a7
syberke1352 <jaisyurrohmanb@gmail.com>
2025-08-11 16:48:58 +0700

    Update Application Title to Bansos

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Sistem Bansos Pendidikan') }}</title>
+    <title>{{ config('app.name', 'Bansos') }}</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
